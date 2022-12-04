00bd07ecc69bf34b612c441e697677d2e4fcb8de
Ismat Alakbarov <66039135+IsmatAl@users.noreply.github.com>
2022-12-04 17:13:16 +0200

    Add integration test for city entity repository

--- README.md
+++ README.md
@@ -19,8 +19,8 @@ gradlew sA clean build -Dorg.gradle.java.home="c:/dev/Java/openjdk-17.0.2"
 
 ## Rest Documentation
 * Documentation of the REST APIs
-* The Swagger UI landing page url, {environment-url}/swagger-ui/index.html
-* Json url, {environment-url}/v3/api-docs, developers can import the json file or url into the postman.
+* The Swagger UI landing page url, {environment-url}/portal/swagger-ui/index.html
+* Json url, {environment-url}/portal/v3/api-docs, developers can import the json file or url into the postman.
 
 ## Actuator
 * Actuator brings production-ready features to our application.
@@ -31,3 +31,6 @@ gradlew sA clean build -Dorg.gradle.java.home="c:/dev/Java/openjdk-17.0.2"
 	* /info
 	* /prometheus
 	* /metrics
+
+##Docker
+In order to make it easier for developers we can containerize database with a docker compose file added under the etc/config/database folder together with init script
--- src/test/java/com/example/citylist/persistence/city/CityEntityRepositoryIntegrationTest.java
+++ src/test/java/com/example/citylist/persistence/city/CityEntityRepositoryIntegrationTest.java
@@ -23,63 +23,60 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @ContextConfiguration(classes = DataJpaTestConfiguration.class)
 public class CityEntityRepositoryIntegrationTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+	@Autowired
+	private TestEntityManager entityManager;
 
-    @Autowired
-    private CityEntityRepository cityEntityRepository;
+	@Autowired
+	private CityEntityRepository cityEntityRepository;
 
-    @Test
-    void shouldSaveCityIfFieldsAreValid() {
-        // given
-        final CityEntity cityEntity = entityManager.persistAndFlush(
-                testCityEntity().build());
-        final CityEntity savedCityEntity = cityEntityRepository.save(cityEntity);
-        entityManager.flush();
+	@Test
+	void shouldSaveCityIfFieldsAreValid() {
+		// given
+		final CityEntity cityEntity = entityManager.persistAndFlush(testCityEntity().build());
+		final CityEntity savedCityEntity = cityEntityRepository.save(cityEntity);
+		entityManager.flush();
 
-        // when
-        final CityEntity actual = cityEntityRepository
-                .getReferenceById(savedCityEntity.getId());
+		// when
+		final CityEntity actual = cityEntityRepository.getReferenceById(savedCityEntity.getId());
 
-        // then
-        assertThat(actual).usingRecursiveComparison().ignoringFields("id", "createdAt", "modifiedAt", "modifiedBy")
-                .isEqualTo(cityEntity);
-        assertThat(actual.getId()).isEqualTo(savedCityEntity.getId());
-        assertThat(actual.getCreatedAt()).isNotNull().isBeforeOrEqualTo(Instant.now());
-        assertThat(actual.getCreatedBy()).isNotNull();
-        assertThat(actual.getModifiedAt()).isNotNull().isBeforeOrEqualTo(Instant.now());
-        assertThat(actual.getModifiedBy()).isNotNull();
-    }
+		// then
+		assertThat(actual).usingRecursiveComparison().ignoringFields("id", "createdAt", "modifiedAt", "modifiedBy")
+				.isEqualTo(cityEntity);
+		assertThat(actual.getId()).isEqualTo(savedCityEntity.getId());
+		assertThat(actual.getCreatedAt()).isNotNull().isBeforeOrEqualTo(Instant.now());
+		assertThat(actual.getCreatedBy()).isNotNull();
+		assertThat(actual.getModifiedAt()).isNotNull().isBeforeOrEqualTo(Instant.now());
+		assertThat(actual.getModifiedBy()).isNotNull();
+	}
 
-    @Test
-    void shouldThrowExceptionIfCityWithNoName() {
-        assertThatThrownBy(() -> {
-            cityEntityRepository.save(testCityEntity().cityName(null).build());
-            entityManager.flush();
-        }).isInstanceOf(ConstraintViolationException.class).hasMessageContaining("City name is required");
-    }
+	@Test
+	void shouldThrowExceptionIfCityWithNoName() {
+		assertThatThrownBy(() -> {
+			cityEntityRepository.save(testCityEntity().cityName(null).build());
+			entityManager.flush();
+		}).isInstanceOf(ConstraintViolationException.class).hasMessageContaining("City name is required");
+	}
 
-    @Test
-    void shouldThrowExceptionIfCityWithNoImageURL() {
-        assertThatThrownBy(() -> {
-            cityEntityRepository.save(testCityEntity().imageURL(null).build());
-            entityManager.flush();
-        }).isInstanceOf(ConstraintViolationException.class).hasMessageContaining("Image URL is required");
-    }
+	@Test
+	void shouldThrowExceptionIfCityWithNoImageURL() {
+		assertThatThrownBy(() -> {
+			cityEntityRepository.save(testCityEntity().imageURL(null).build());
+			entityManager.flush();
+		}).isInstanceOf(ConstraintViolationException.class).hasMessageContaining("Image URL is required");
+	}
 
-    @Test
-    void shouldReturnCities() {
-        // given
-        final CityEntity cityEntity1 = cityEntityRepository.saveAndFlush(testCityEntity().build());
+	@Test
+	void shouldReturnCities() {
+		// given
+		final CityEntity cityEntity1 = cityEntityRepository.saveAndFlush(testCityEntity().build());
 
-        // when
-        final Page<City> actual = cityEntityRepository.findCities(null, null, PageRequest.of(0, 1));
+		// when
+		final Page<City> actual = cityEntityRepository.findCities(null, null, PageRequest.of(0, 1));
 
-        // then
-        assertThat(actual.getContent()).hasSize(1);
-        assertThat(actual.getContent()).extracting(City::getId).containsExactly(cityEntity1.getId());
-        assertThat(actual.getContent()).extracting(City::getCityName).contains(cityEntity1.getCityName());
-        assertThat(actual.getContent()).extracting(City::getImageURL)
-                .containsExactly(cityEntity1.getImageURL());
-    }
+		// then
+		assertThat(actual.getContent()).hasSize(1);
+		assertThat(actual.getContent()).extracting(City::getId).containsExactly(cityEntity1.getId());
+		assertThat(actual.getContent()).extracting(City::getCityName).contains(cityEntity1.getCityName());
+		assertThat(actual.getContent()).extracting(City::getImageURL).containsExactly(cityEntity1.getImageURL());
+	}
 }
